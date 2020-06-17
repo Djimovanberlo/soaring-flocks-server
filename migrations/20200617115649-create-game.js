55ad76e2800ef39db4dc733901bdd5d4d40df1d0
Djimovanberlo <djimovanberlo@gmail.com>
2020-06-17 13:58:56 +0200

    game model

---
+++ migrations/20200617115649-create-game.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      gameTitle: {
+        type: Sequelize.STRING
+      },
+      gameTime: {
+        type: Sequelize.INTEGER
+      },
+      gameTimePassed: {
+        type: Sequelize.INTEGER
+      },
+      closed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('games');
+  }
+};
