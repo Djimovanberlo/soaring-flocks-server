f005417007958abcf68f940a56d8f461c20a897e
Djimovanberlo <djimovanberlo@gmail.com>
2020-07-06 12:28:36 +0200

    setup bigFunction on interval

---
+++ src/bigFunctionTimer.js
@@ -0,0 +1,74 @@
+const Resource = require("../models").resource;
+const Player = require("../models").player;
+const Game = require("../models").game;
+const PrivateMessage = require("../models").privateMessage;
+
+async function bigFunction() {
+  const activePlayers = await Player.findAll({
+    where: {
+      inGame: true,
+    },
+  });
+
+  activePlayers.forEach((player) => {
+    const {
+      moneyCash,
+      egg,
+      feather,
+      bug,
+      vPoint,
+      mMarket,
+      rMarket,
+      vMarket,
+    } = player.dataValues;
+
+    for (let i = 0; i < rMarket; i++) {
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+      const resource = getRandomInt(3);
+      if (resource === 0) {
+        player.update({
+          egg: player.egg + 1,
+        });
+      } else if (resource === 1) {
+        player.update({
+          feather: player.feather + 1,
+        });
+      } else if (resource === 2) {
+        player.update({
+          bug: player.bug + 1,
+        });
+      }
+    }
+    player.update({
+      moneyCash: player.moneyCash + mMarket * 2,
+    });
+  });
+  const allGames = await Game.findAll();
+  allGames.forEach((game) => {
+    console.log(game);
+    game.update({
+      gameTime: game.gameTime - 1,
+      gameTimePassed: game.gameTimePassed + 1,
+    });
+  });
+}
+
+// bigFunction();
+const date = new Date();
+console.log(date, date.getHours());
+
+// const testLog = () => {
+//   console.log("HALLO")
+// }
+// const myInterval = setInterval(testLog, 2000)
+const intervalTest = () => {
+  setInterval(function () {
+    const date = new Date();
+    if (date.getHours() === 12 && date.getMinutes() === 28) {
+      console.log("HALLO", date);
+    }
+  }, 6000);
+};
+intervalTest();
