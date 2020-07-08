e66ffd4f1f4eb7a2727aef12d964201daacdd708
Djimovanberlo <djimovanberlo@gmail.com>
2020-07-08 14:24:14 +0200

    updated startGame function

--- src/startGame.js
+++ src/startGame.js
@@ -2,14 +2,22 @@ const Game = require("../models").game;
 
 async function startGame() {
   try {
-    const startedGame = await Game.create({
-      id: 1,
-      gameTitle: "Soaring Flocks",
-      gameTime: 20,
-      gameTimePassed: 0,
-      closed: false,
+    const foundGame = await Game.findOne({
+      where: {
+        id: 1,
+      },
     });
-    console.log("CREATED GAME", startedGame);
+    console.log("FOUND GAME", foundGame);
+    if (!foundGame) {
+      const startedGame = await Game.create({
+        id: 1,
+        gameTitle: "Soaring Flocks",
+        gameTime: 20,
+        gameTimePassed: 0,
+        closed: false,
+      });
+      console.log("CREATED GAME", startedGame);
+    }
   } catch (err) {
     console.log(err);
   }
