d5eb042c6523d053cd2b17d6efdd43954ae3fcd0
Djimovanberlo <djimovanberlo@gmail.com>
2020-09-11 17:38:51 +0200

    test remove try{}catch{} from endGame && endTurn functions

--- src/endGame.js
+++ src/endGame.js
@@ -4,26 +4,26 @@ const PublicMessage = require("../models").publicMessage;
 const Trade = require("../models").trade;
 
 async function endGame() {
-  try {
-    await Player.destroy({
-      where: {},
+  // try {
+  await Player.destroy({
+    where: {},
+  });
+  await PublicMessage.destroy({
+    where: {},
+  });
+  await Trade.destroy({
+    where: {},
+  });
+  const allGames = await Game.findAll();
+  allGames.forEach((game) => {
+    game.update({
+      gameTime: 15,
+      gameTimePassed: 0,
     });
-    await PublicMessage.destroy({
-      where: {},
-    });
-    await Trade.destroy({
-      where: {},
-    });
-    const allGames = await Game.findAll();
-    allGames.forEach((game) => {
-      game.update({
-        gameTime: 15,
-        gameTimePassed: 0,
-      });
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 }
 
 module.exports.endGame = endGame;
